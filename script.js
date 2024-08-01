56726e0382e6636dad1436c6358ff3993864caac
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2024-08-01 11:42:46 +0100

    Done

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('fetchQuotes')
+btn.addEventListener('click',()=>{
+    const url = 'https://api.quotable.io/random'
+    fetch(url)
+    .then(response=>response.json())
+    .then((result)=>{
+        console.log(result);
+        show.innerHTML = `
+            <h4>${result.author}</h4>
+            <h2><i>${result.content}</i></h2>
+        `
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
